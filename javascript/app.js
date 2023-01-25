58a926f61f500a8b0b64405c3b3b024be9bae86d
John Cokos <highlander.44@gmail.com>
2023-01-24 20:48:02 -0800

    Adds a rateUs function with a star system

--- javascript/app.js
+++ javascript/app.js
@@ -14,3 +14,18 @@ function getName() {
   let message = "Welcome Back! " + name;
   return message;
 }
+
+function rateUs() {
+
+  let rating = prompt("On a scale of 1-5, how much do you love our cookies?");
+
+  while (rating < 5) {
+    rating = prompt("Are you sure? Try a bigger number, please!");
+    rating = Number(rating);
+  }
+  for (let i = 1; i <= rating; i++) {
+    document.write("⭐️");
+    // document.write("<img src='star.png' width='20' />")
+  }
+
+}
